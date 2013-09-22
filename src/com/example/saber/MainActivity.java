28b51a18afb75dabbc8d1045da5655406c32af38
pp <KAVEH@Kaveh_VAIO.mshome.net>
2013-09-22 14:19:27 +0330

    t2

--- src/com/example/saber/MainActivity.java
+++ src/com/example/saber/MainActivity.java
@@ -10,8 +10,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		//jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
-	}
+		//jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjkkkkkkkkkkkkkkkkkkk
+		}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
